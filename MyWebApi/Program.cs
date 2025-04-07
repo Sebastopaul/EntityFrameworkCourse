c2c250514c7e7db400ba07189df9140c8b04ccc7
Sébastien Lamard <sebastien.lamard.pro@gmail.com>
2025-04-07 16:35:00 +0200

    fix(program): enable sensitive data logging only on dev

--- MyWebApi/Program.cs
+++ MyWebApi/Program.cs
@@ -13,23 +13,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    if (!options.IsConfigured)
-    {
-        options
-            .UseSqlServer(builder.Configuration.GetConnectionString("Isitech"))
-            .EnableSensitiveDataLogging()
-            .LogTo(Console.WriteLine, LogLevel.Information)
-            .EnableDetailedErrors();
-    }
-});
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    if (!options.IsConfigured)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            options.EnableSensitiveDataLogging()
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableDetailedErrors();
+        }
+
+        options.UseSqlServer(builder.Configuration.GetConnectionString("Isitech"));
+    }
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
